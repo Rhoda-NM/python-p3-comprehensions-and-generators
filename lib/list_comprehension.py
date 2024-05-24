1818dae52ab82b3716beee9265674ce441adf51c
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-05-24 13:47:27 +0300

    list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 == 0]
+    return (evens)
 
 def make_exclamation(sentence_list):
-    pass
+    sentences = [(f"{sentence}!") for sentence in sentence_list]
+    return sentences
+s = ["I like computers", "I require coffee", "Live long and prosper"]
+print(make_exclamation(s))
